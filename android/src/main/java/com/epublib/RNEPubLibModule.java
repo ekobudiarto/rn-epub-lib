97ea124257c4a1b84efe5c61f62122a989d49238
ekobudiarto <ekobudiartoy@gmail.com>
2019-03-18 11:29:36 +0700

    Update Module

---
+++ android/src/main/java/com/epublib/RNEPubLibModule.java
@@ -0,0 +1,40 @@
+
+package com.epublib;
+
+import android.widget.Toast;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RNEPubLibModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+  private static final String DURATION_SHORT_KEY = "SHORT";
+  private static final String DURATION_LONG_KEY = "LONG";
+
+  public RNEPubLibModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+    constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+    return constants;
+  }
+
+  @Override
+  public String getName() {
+    return "RNEPubLib";
+  }
+
+  @ReactMethod
+  public void show(String message, int duration) {
+    Toast.makeText(getReactApplicationContext(), message, duration).show();
+  }
+}
--- android/src/main/java/com/epublib/RNEPubLibPackage.java
+++ android/src/main/java/com/epublib/RNEPubLibPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.epublib;
 
 import java.util.Arrays;
 import java.util.Collections;
